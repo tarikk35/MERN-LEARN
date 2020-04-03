f791d01b415bbc5443745cf13bd6f6cbd4f5a615
Tarık Köprülü <tarikk3545@gmail.com>
2020-04-03 19:49:48 +0300

    Added database connection and crucial packages

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to the DB.");
+  } catch (err) {
+    console.error(err.message);
+    // Exit because of failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
