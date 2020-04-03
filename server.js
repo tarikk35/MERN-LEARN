d5341f27fd1a0950628de75579b7f1c10bbbe634
Tarık Köprülü <tarikk3545@gmail.com>
2020-04-03 20:09:38 +0300

    Added basic routes

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get("/", (req, res) => {
   res.send("API Running M8");
 });
 
+
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port :${PORT}`);
 });
